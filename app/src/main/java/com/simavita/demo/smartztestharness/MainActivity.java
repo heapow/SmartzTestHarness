7bc5499b5bf32ae4b6651ea3fabe82080113035f
Wayne <wangw@shinetechchina.com>
2021-05-31 13:42:37 +0800

    Update the DataAnalytics.aar

--- app/src/main/java/com/simavita/demo/smartztestharness/MainActivity.java
+++ app/src/main/java/com/simavita/demo/smartztestharness/MainActivity.java
@@ -15,7 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DataInfo info = DataAnalytics.smartzsdk.processdata(new byte[]{0x01, 0x02, 0x03, 0x04});
+        DataInfo info = DataAnalytics.smartzsdk.processdata("1EFFB0A0F2DE3C14209E2DAAEAE9BD17C098BED8FFDE1DC11E52D4265A09061809536D6172747A5F506F645F46343930434244303035354305FFCDAB0100".getBytes(),
+                new DataInfo(0,0,0,0,0,0,0,
+                        0,0,0,0,
+                        0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0));
+
         Log.d("datainfo", info.toString());
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(info.toString());
