b83fc6f7b70ae90339327bf0660869eb5f1c0d5b
Wayne <wangw@shinetechchina.com>
2021-05-31 14:57:38 +0800

    DataAnalytics Unit Test template

---
+++ app/src/androidTest/java/com/simavita/demo/smartztestharness/DataAnalyticsInstrumentedTest.java
@@ -0,0 +1,38 @@
+package com.simavita.demo.smartztestharness;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.simavita.DataInfo;
+import com.simavita.sdk.dataanalytics.DataAnalytics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class DataAnalyticsInstrumentedTest {
+    @Test
+    public void processdata() {
+        DataInfo info = DataAnalytics.smartzsdk.processdata("1EFFB0A0F2DE3C14209E2DAAEAE9BD17C098BED8FFDE1DC11E52D4265A09061809536D6172747A5F506F645F46343930434244303035354305FFCDAB0100".getBytes(),
+                new DataInfo(0,0,0,0,0,0,0,
+                        0,0,0,0,
+                        0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0));
+
+    }
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.simavita.demo.smartztestharness", appContext.getPackageName());
+    }
+    @Test
+    public  void test() {
+        assertEquals(0, 0);
+//        assertEquals(1, 0);
+    }
+}
