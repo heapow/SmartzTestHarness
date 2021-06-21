877545b9a053cca9dd90faeaf965812e17a9f951
Wayne <wangw@shinetechchina.com>
2021-06-21 17:08:56 +0800

    Update dependencies and Test Harness

--- app/src/main/java/com/simavita/demo/smartztestharness/AnalyticsPanel.java
+++ app/src/main/java/com/simavita/demo/smartztestharness/AnalyticsPanel.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 
 import com.example.simavita.DataInfo;
+import com.example.simavita.VolumeInfo;
+import com.example.simavita.VolumeStatusInfo;
+import com.simavita.sdk.dataanalytics.DataAnalytics;
 
 public class AnalyticsPanel extends LinearLayout {
 
@@ -32,15 +35,21 @@ public class AnalyticsPanel extends LinearLayout {
     TextView mAy;  // ay
     TextView mAz;  // az
     TextView mPitchAngle;  // PitchAngle
-    TextView mVoltage; // vol
-    TextView mBatteryPercentage; //batt
-    TextView mBatteryStatus; //stat
+    TextView mVolume; // vol
+    TextView mVoltage; //batt
+    TextView mPodStatus; //stat
     TextView mTemperature; // temp
     TextView mRawTemperature;
     TextView mTimeInPad; // padT
     TextView mTimeInPosition; //posT
     TextView mTimeInFall;
+    TextView mSensorValue; // sensor
 
+    TextView mPodVersion; // pod version
+
+    TextView mPPM;
+    TextView mMinPPM;
+    TextView mMaxPPM;
     // IntrpAx
     // IntrpAy
     // IntrpAz
@@ -88,18 +97,24 @@ public class AnalyticsPanel extends LinearLayout {
          mAy = layout.findViewById(R.id.mAy);  // ay
          mAz = layout.findViewById(R.id.mAz);  // az
          mPitchAngle = layout.findViewById(R.id.mPitchAngle);  // PitchAngle
-         mVoltage = layout.findViewById(R.id.mVoltage); // vol
-         mBatteryPercentage = layout.findViewById(R.id.mBatteryPercentage); //batt
-         mBatteryStatus = layout.findViewById(R.id.mBatteryStatus); //stat
+         mVolume = layout.findViewById(R.id.mVolume); // vol
+         mVoltage = layout.findViewById(R.id.mVoltage); //batt
+         mPodStatus = layout.findViewById(R.id.mPodStatus); //stat
          mTemperature = layout.findViewById(R.id.mTemperature); // temp
          mTimeInPad = layout.findViewById(R.id.mTimeInPad); // padT
          mTimeInPosition = layout.findViewById(R.id.mTimeInPosition); //posT
+         mSensorValue = layout.findViewById(R.id.mSensorValue); //sensor
+         mPodVersion = layout.findViewById(R.id.mPodVersion); //podVersion
+
+        mPPM = layout.findViewById(R.id.mPPM); //mPPM
+        mMinPPM = layout.findViewById(R.id.mMinPPM); //mMinPPM
+        mMaxPPM = layout.findViewById(R.id.mMaxPPM); //mMaxPPM
     }
     public void showDataInfo(DataInfo info) {
         if (info == null) return;
         mConnectionStatus.setText(String.valueOf(info.getConnectionStatus()));
-        mPosition.setText(String.valueOf(info.getPosition()));
-        mFall.setText(String.valueOf(info.getFall()));
+        mPosition.setText(DataAnalytics.getPositionLabel(info.getPosition()));
+        mFall.setText(DataAnalytics.getFallLabel(info.getFall()));
         mHardwareVersion.setText(String.valueOf(info.getHardwareVersion()));
         mSensor1Value.setText(String.valueOf(info.getSensor1Value()));
         mSensor2Value.setText(String.valueOf(info.getSensor2Value()));
@@ -112,11 +127,31 @@ public class AnalyticsPanel extends LinearLayout {
         mAz.setText(String.valueOf(info.getAz()));
         mPitchAngle.setText(String.valueOf(info.getPitchAngle()));
         mVoltage.setText(String.valueOf(info.getVoltage()));
-        mBatteryPercentage.setText(String.valueOf(info.getBatteryPercentage()));
-        mBatteryStatus.setText(String.valueOf(info.getBatteryStatus()));
+
         mTemperature.setText(String.valueOf(info.getTemperature()));
         mTimeInPad.setText(String.valueOf(info.getTimeInPad()));
         mTimeInPosition.setText(String.valueOf(info.getTimeInPosition()));
+        mPodVersion.setText(String.valueOf(info.getFirmwareVersion()));
+        mSensorValue.setText(String.valueOf(info.getAggerateValue()));
+
+
+//        mPodStatus.setText(String.valueOf(info.getBatteryStatus()));
+//        mVolume
+        // Urine
+        // Hydration
+        // B/P ignore
 
     }
+    public void showVolumeInfo(VolumeStatusInfo info) {
+//        mPodStatus.setText(String.valueOf(info.getPodStatus()));
+        mPodStatus.setText(DataAnalytics.getStatusLabel(info.getPodStatus()));
+        mVolume.setText(String.valueOf(info.getActualVolume()));
+    }
+
+    public void showPPM(double ppm, double min, double max) {
+        mPPM.setText(String.format("%.2f", ppm));
+        mMinPPM.setText(String.format("%.2f", min));
+        mMaxPPM.setText(String.format("%.2f", max));
+    }
+    // vol
 }
--- app/src/main/java/com/simavita/demo/smartztestharness/MainActivity.java
+++ app/src/main/java/com/simavita/demo/smartztestharness/MainActivity.java
@@ -14,11 +14,24 @@ import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.example.simavita.DataInfo;
+import com.example.simavita.PreviousDataInfo;
+import com.example.simavita.VolumeInfo;
+import com.example.simavita.VolumeStatusInfo;
 import com.simavita.sdk.dataanalytics.DataAnalytics;
+import com.simavita.sdk.guprofile.UserApi;
+import com.simavita.sdk.guprofile.bean.Group;
+import com.simavita.sdk.guprofile.bean.Patient;
 import com.simavita.sdk.pod.ble.data.DataAcquisition;
 import com.simavita.sdk.pod.ble.data.demo.RuntimePermissionChecker;
+import com.simiavita.sdk.mpcatalog.ConfigurationApi;
+import com.simiavita.sdk.mpcatalog.bean.Configuration;
+import com.simiavita.sdk.mpcatalog.bean.Manufacturer;
+import com.simiavita.sdk.mpcatalog.bean.Product;
+import com.simiavita.sdk.mpcatalog.bean.Threshold;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_ENABLE_BT = 1;
     private static final int REQUEST_LOCATION_PERMISSION = 1;
     private AnalyticsPanel analyticsPanel;
+//    private final String TEST_POD = "Smartz_Pod_F490CBD0056C";
+    private final String TEST_POD = "Smartz_Pod_F490CBD000A5";
+    private  DataInfo previousInfo = DataAnalytics.getDefaultDataInfo();
+    private VolumeStatusInfo previousVolumeStatusInfo = null;
+    private List<Threshold> thresholds = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         if (permissionChecker.checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION, "", REQUEST_LOCATION_PERMISSION)) {
             initDataAcquisition();
         }
+        prepareThreshold();
 //        initDataAcquisition();
     }
     public void initDataAcquisition() {
@@ -59,21 +78,128 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void calculatePPM(DataAcquisition.DataHandler data) {
                 Log.d("TestAct PPM", Double.toString(data.GetMinPPM())+ " | " + Double.toString(data.GetMaxPPM()));
-                double ppm1 = data.GetPPM("Smartz_Pod_44444444");
-                double ppm2 = data.GetPPM("44:44:44:44");
+                double ppm = data.GetPPM(TEST_POD);
+//                double ppm2 = data.GetPPM("44:44:44:44");
                 double minPPM = data.GetMinPPM();
                 double maxPPM = data.GetMaxPPM();
+                analyticsPanel.showPPM(ppm, minPPM, maxPPM);
             }
 
             @Override
             public void onLeScan(String data, BluetoothDevice device, int rssi, byte[] scanRecord) {
-                Log.d("DataInfo", data.toString());
-                DataInfo info = DataAnalytics.smartzsdk.processdata(scanRecord, DataAnalytics.getDefaultDataInfo());
+                Log.d("BLE Device", device.getName());
+                if (!TEST_POD.equals(device.getName())) return;
+                DataInfo info = DataAnalytics.smartzsdk.processdata(scanRecord, previousInfo);
+
                 analyticsPanel.showDataInfo(info);
+                try{
+                    algorithm(previousInfo, info);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (info != null) previousInfo = info;
             }
         });
         Log.d("Tag", "initDataAcquisition");
     }
+
+    private void algorithm(DataInfo previous, DataInfo current) {
+        if (current == null || thresholds == null) return;
+        List<Threshold> locThresholds = new ArrayList<>();
+        for(Threshold thre : thresholds) {
+            if(thre.getFwVersion().equals(current.getFirmwareVersion())
+                    && thre.getHwVersion().equals(current.getHardwareVersion())) {
+                locThresholds.add(thre);
+            } else {
+                locThresholds.add(thre);
+            }
+        }
+
+        // Step 6: Filter out the threshold for a position that the pod is
+        // in. (Position can be obtained from the DataInfo)
+        Threshold threshold = null;
+        for(Threshold thre : locThresholds) {
+            if (current.getPosition() == thre.getPosition()) {
+                threshold = thre;
+            }
+        }
+        if (threshold == null)  threshold = locThresholds.get(0);
+        VolumeInfo volumeInfo = new VolumeInfo(threshold.getConstantA()
+                ,threshold.getConstantB()
+                ,threshold.getConstantC()
+                ,threshold.getConstantD()
+                ,threshold.getYellowThreshold()
+                ,threshold.getRedThreshold()
+                ,threshold.getMaxCapacity()
+                ,50
+                ,25);
+
+        // Parameter 2: sensorValue
+        int sensorValue = current.getAggerateValue();
+
+        // Parameter 3: PreviousDataInfo
+        int preVolum = 0;
+        int prePadStatus = 0;
+        if (previousVolumeStatusInfo != null) {
+            preVolum = previousVolumeStatusInfo.getActualVolume();
+            prePadStatus = previousVolumeStatusInfo.getPodStatus();
+        }
+        PreviousDataInfo previousDataInfo = DataAnalytics.getPreviousDataInfo(previous,
+                preVolum, prePadStatus, current);
+        VolumeStatusInfo volumeStatusInfo = DataAnalytics.smartzsdk.getvolume(volumeInfo
+                , sensorValue,
+                previousDataInfo);
+        if (volumeStatusInfo != null) previousVolumeStatusInfo = volumeStatusInfo;
+        if (volumeStatusInfo == null) return;
+        Log.d("volumeStatusInfo", volumeStatusInfo.toString());
+        analyticsPanel.showVolumeInfo(volumeStatusInfo);
+    }
+
+    private void prepareThreshold() {
+        new Thread(){
+            public void run() {
+                try {
+                    // Step 1: Receive user information from GetAllPatients api.
+                    List<Group> groups = UserApi.getAllPatients("eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImQzMzljNDNlLTAzNTEtNGJhZi05YTNlLTdhOWM4YjUyYzljZSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJ0ZXN0c3UxIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZW1haWxhZGRyZXNzIjoidEB0LmNvbSIsImV4cCI6MTY1NTI2NDY4NywiaXNzIjoic2ltYXZpdGEuY29tIiwiYXVkIjoiYXVkaWVuY2UifQ.icTGzCtDCs8Qdbe9i_oYck4PE91EwvGkxixK6OkPtKk");
+
+                    // Step 2: For now, choose any one of the patients of any group.
+                    if(groups == null || groups.size() <= 0) return;
+                    Group group = groups.get(0);
+                    if (group == null) return;;
+                    List<Patient> patients = group.getPatients();
+                    if(patients == null || patients.size() <=0) return;
+                    Patient patient = patients.get(0);
+                    if(patient == null) return;
+
+                    // Step 3: Get Manufacturers information from GetConfiguration api.
+                    Configuration conf = ConfigurationApi.getConfigurationLatestRelease();
+
+                    // Step 4: Filter out the specific product  through the product id in patient details.
+                    List<Product> products = new ArrayList<>();
+                    for(Manufacturer man : conf.getManufacturers()) {
+                        for(Product pro : man.getProducts()) {
+                            if (patient.getDayProductId() == pro.getId() || patient.getNightProductId() == pro.getId()) {
+                                products.add(pro);
+                            }
+                        }
+                    }
+                    if (products.size() <= 0) return;
+                    Product product = products.get(0);
+                    thresholds = new ArrayList<>();
+                    // Step 5: Get product thresholds of that specific product
+                    // (Filter with a firmware version and hardware version which is available from
+                    // DataInfo class. The result  is a list of thresholds for different positions).
+
+                    for(Threshold thre : product.getProductThresholds()) {
+                        thresholds.add(thre);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
